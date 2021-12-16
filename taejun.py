b1fe5e4e4fecafb6e53a3c61a3d51207a04cbcb8
dongriDK <45909972+dongriDK@users.noreply.github.com>
2021-12-16 10:11:50 +0900

    12.16

--- taejun.py
+++ taejun.py
@@ -299,16 +299,16 @@ async def on_message(message):
     await bot.process_commands(message)
     return 0
 
-@bot.event
-async def on_message_delete(message):
-    # embed = discord.Embed(description=message.content + " 삭제됨")
-    channel = bot.get_channel(894545802247159808)
-    await channel.send("'" + message.content + "'" + " 메시지 삭제됨")
+# @bot.event
+# async def on_message_delete(message):
+#     # embed = discord.Embed(description=message.content + " 삭제됨")
+#     channel = bot.get_channel(894545802247159808)
+#     await channel.send("'" + message.content + "'" + " 메시지 삭제됨")
 
-@bot.event
-async def on_message_edit(before, after):
-    channel = bot.get_channel(894545802247159808)
-    await channel.send("'" + before.content + "'" + " -> " + "'" + after.content + "'" + "변경됨")
+# @bot.event
+# async def on_message_edit(before, after):
+#     channel = bot.get_channel(894545802247159808)
+#     await channel.send("'" + before.content + "'" + " -> " + "'" + after.content + "'" + "메시지 변경됨")
 
 @bot.event
 async def on_command_error(ctx, error):
