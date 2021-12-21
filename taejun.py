b1a2a96a1eb47061659e8a89c389f3a11ec9c0d8
dongriDK <45909972+dongriDK@users.noreply.github.com>
2021-12-21 16:11:14 +0900

    12.21

--- taejun.py
+++ taejun.py
@@ -96,8 +96,6 @@ def DbModify_text(message, con, cur):
 def DbModify_voice(member, before, after, con, cur):
     retValue = 0
     totalSeconds = 0
-    print(before)
-    print(after)
     try:
         beChannel = "없음" if before.channel == None else before.channel.name.split("＿")[1]
     except:
@@ -143,8 +141,7 @@ def DbModify_voice(member, before, after, con, cur):
 
     return retValue, beChannel, totalSeconds
 
-def DbModify_user_info(bename, afname, betag, aftag, id, con, cur):
-    print(afname, aftag, id)
+def DbModify_user_info(afname, aftag, id, con, cur):
     cur.execute("UPDATE login set name=%s, tag=%s where id=%s", (afname, aftag, id,))
     cur.execute("UPDATE user_info set name=%s, tag=%s where id=%s", (afname, aftag, id,))
     con.commit()
@@ -361,8 +358,8 @@ async def on_user_update(before, after):
     if (beName != afName):
         msg = "`" + beName + "` -> `" + afName + "` 디스코드 아이디 변경"
         if (beDis != afDis):
-            msg = "`" + beDis + "` -> `" + afDis + "` 디스코드 태그 변경"
-        DbModify_user_info(beName, afName, beDis, afDis, before.id, con, cur)
+            msg = "`" + beName + "` " + "`" + beDis + "` -> `" + afDis + "` 디스코드 태그 변경"
+        DbModify_user_info(afName, afDis, before.id, con, cur)
         await SendMessage(taejunRoom, msg)
 
 @bot.event
