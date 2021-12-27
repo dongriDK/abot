6905731d925a475c661b047e05e7aaf5826cb82c
dongriDK <45909972+dongriDK@users.noreply.github.com>
2021-12-27 14:47:10 +0900

    12.27

--- taejun.py
+++ taejun.py
@@ -371,8 +371,10 @@ async def on_voice_state_update(member, before, after):
 @bot.event
 async def on_message(message):
     con, cur = DbConnect()
+    print(message.author.name)
     if (message.author.name != "태준이" or message.author.name != "InFi-EYE"):
         if("!ㅌ" not in message.content):
+            print(message.author.name)
             DbReturn = DbLogin(message.author.id, message.author.name, message.author.discriminator, con, cur)
             DbModify_text(message, con, cur)
 
