42be8fbabd3fbd4e7f1df012eac78e2b80174b01
dongriDK <45909972+dongriDK@users.noreply.github.com>
2022-02-23 17:15:31 +0900

    edit requests

--- taejun.py
+++ taejun.py
@@ -10,7 +10,7 @@ from discord.ext import commands
 from discord.ext.commands.errors import CommandInvokeError
 from discord.ext.commands import CommandNotFound
 from discord.ext import tasks
-# import requests
+import requests
 
 intents = discord.Intents.default()
 intents.members = True
