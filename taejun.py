44f1cabc53d1f4453e75af5160006e835b62aaa4
dongriDK <45909972+dongriDK@users.noreply.github.com>
2022-03-11 00:06:38 +0900

    Revert "edit total end"
    This reverts commit b866ac1239e81413badc2d8813fb01db0165ff5f.

--- taejun.py
+++ taejun.py
@@ -427,8 +427,7 @@ async def on_message(message):
                 res = requests.get(APEX_URL + name + APEX_TOKEN)
                 json_data = json.loads(res.text)
                 embed = await ParsingJson(name, json_data, message.channel)
-                if (embed != 0):
-                    await message.channel.send(embed = embed)
+                await message.channel.send(embed = embed)
         
         
 
