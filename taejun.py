26c10c6014b5da11c926518240c113a8e7ea47eb
dongriDK <45909972+dongriDK@users.noreply.github.com>
2023-01-20 12:04:18 +0900

    보여주는 행 수 수정

--- taejun.py
+++ taejun.py
@@ -296,7 +296,7 @@ def MakeMention(id, tag):
     
 
 def MakePageList(channel, list_, flag, arg, arg1):
-    showlist = 20
+    showlist = 15
     disc_list = []
     pages = []
     total_len = len(list_)
